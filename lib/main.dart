0bfddded47ec2946fc6e20fa69740721739be6bb
Dawnny <haidawng205@gmail.com>
2022-04-29 21:29:09 +0700

    setup webfirebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,31 @@
+import 'package:endterm/providers/user_provider.dart';
+import 'package:endterm/responsive/mobile_screen_layout.dart';
+import 'package:endterm/responsive/responsive_layout.dart';
+import 'package:endterm/responsive/web_screen_layout.dart';
+import 'package:endterm/views/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:endterm/constants/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:endterm/views/signup_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() async {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
+  if (kIsWeb){
     await Firebase.initializeApp(
-        options: const FirebaseOptions( //lúc tạo web app trên firebase sẽ đc cung cấp cái này
-            apiKey: "AIzaSyCVX1-5K7ehw5aZrE2Gv1F-rv44f5HNgJs",
-            authDomain: "instagram-flutter-7c654.firebaseapp.com",
-            projectId: "instagram-flutter-7c654",
-            storageBucket: "instagram-flutter-7c654.appspot.com",
-            messagingSenderId: "368117043222",
-            appId: "1:368117043222:web:6f1b3c2898c64c0ce62537"
-        ));
-        }
-        else{
+        options: const FirebaseOptions(         //lúc tạo web app trên firebase sẽ đc cung cấp cái này
+            apiKey: 'AIzaSyBpgYdaQULiNgONN1JGkFko6eP9wmDSCpI',
+            appId: '1:564771532215:web:2f43d41be689dcbee70c32',
+            messagingSenderId: '564771532215',
+            projectId: 'endterm-group9',
+            storageBucket: 'endterm-group9.appspot.com')
+    );
+  }
+  else{
     await Firebase.initializeApp();
-    }
-        runApp(const MyApp());
+  }
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -30,26 +34,47 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'SocialHub',
-      theme: ThemeData.dark().copyWith(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram',
+        theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: mobileBackgroundColor
-      ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        //userChanges() thì sẽ được gọi mỗi khi user sign in, sign out,thay nổi password,email, ...
-        //idTokenChanges() thì sẽ được gọi mỗi khi user sign in, sign out,... điểm trừ là nếu ng dùng cài app đó
-        //lên máy khác(apk) thì app đó sẽ ghi nhớ luôn phiên đăng nhập trong máy trước
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(color: Colors.white,),
-            );
-          }
-
-          return const SignUpScreen();
-        },
+        ),
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          //userChanges() thì sẽ được gọi mỗi khi user sign in, sign out,thay nổi password,email, ...
+          //idTokenChanges() thì sẽ được gọi mỗi khi user sign in, sign out,... điểm trừ là nếu ng dùng cài app đó 
+          //lên máy khác(apk) thì app đó sẽ ghi nhớ luôn phiên đăng nhập trong máy trước
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active){    //active là check xem đã kết nối đc tới stream ở trên hay chưa
+              if (snapshot.hasData){
+                return const ResponsiveLayout(
+                  webScreenLayout: WebScreenLayout(),
+                  mobileScreenLayout: MobileScreenLayout(),
+                );
+              }
+              else if (snapshot.hasError){
+                return Center(
+                  child: Text('${snapshot.error}'),
+                );
+              }
+            }
+    
+            if (snapshot.connectionState == ConnectionState.waiting){
+              return const Center(
+                child: CircularProgressIndicator(color: Colors.white,),
+              );
+            }
+    
+            return const LoginScreen();
+          },
+        ),
       ),
     );
   }
